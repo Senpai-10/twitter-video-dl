83b4bff16208acb5b38601d1c8ad6c6cab9f9c4b
abdalrzag eisa <bmjfdrh@gmail.com>
2022-08-15 03:39:44 +0300

    accept multiple urls

--- twitter-video-dl.py
+++ twitter-video-dl.py
@@ -1,5 +1,13 @@
 import src.twitter_video_dl.twitter_video_dl as tvdl
 import argparse
+import string
+import random
+
+def random_file_name_genrator() -> str: 
+    list = string.ascii_letters + string.digits
+    length = 10
+    
+    return ''.join(random.choice(list) for i in range(length)) + ".mp4"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -7,19 +15,26 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "twitter_url",
-        type=str,
+        "urls",
+        nargs="+",
         help="Twitter URL to download.  e.g. https://twitter.com/GOTGTheGame/status/1451361961782906889"
     )
 
-    parser.add_argument(
-        "file_name",
-        type=str,
-        help="Save twitter video to this filename. e.g. twittervid.mp4"
-    )
+    # parser.add_argument(
+    #     "--file_name",
+    #     type=str,
+    #     help="Save twitter video to this filename. e.g. twittervid.mp4",
+    #     required=False
+    # )
 
     args = parser.parse_args()
 
-    file_name = args.file_name if args.file_name.endswith(".mp4") else args.file_name + ".mp4"
+    # file_name = args.file_name if args.file_name.endswith(".mp4") else args.file_name + ".mp4"
 
-    tvdl.download_video(args.twitter_url, file_name)
+    for url in args.urls:
+        file_name = random_file_name_genrator()
+        
+        print(f"Downloading '{url}'")
+        tvdl.download_video(url, file_name)
+        print("Done!")
+    
